ec87687f98716d559373d991f7a63ec4b7c77355
Daniil Ovsyannikov <to.djet@gmail.com>
2020-12-15 08:02:11 +0300

    0.2.2 fixed healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 # VCHOST=$(cat "/var/lib/vconnector/hosts.file" | grep -oP '^[^;]+')
-vpoller-client -m about -V $VPOLLER_VC_HOST | grep -c '"success": 0'
+
+EXITCODE=0
+if [ ! -z ${VPOLLER_VC_HOST_01} ]; then
+    vpoller-client -m about -V $VPOLLER_VC_HOST_01 | grep -c '"success": 0'
+    if [ ! $? -eq 0 ]; then
+        EXITCODE=1
+    fi
+fi
+if [ ! -z ${VPOLLER_VC_HOST_02} ]; then
+    vpoller-client -m about -V $VPOLLER_VC_HOST_02 | grep -c '"success": 0' 
+    if [ ! $? -eq 0 ]; then
+        EXITCODE=1
+    fi   
+fi
+if [ ! -z ${VPOLLER_VC_HOST_03} ]; then
+    vpoller-client -m about -V $VPOLLER_VC_HOST_03 | grep -c '"success": 0'   
+    if [ ! $? -eq 0 ]; then
+        EXITCODE=1
+    fi 
+fi
+return $EXITCODE
